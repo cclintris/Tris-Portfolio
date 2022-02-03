b01bf06a30b7ad9ec3fed6bd9ec952845d0b6a62
cclintris <181250083@smail.nju.edu.cn>
2022-02-03 23:20:36 +0800

    fix: update portfolio page useState name

--- src/pages/Portfolios.tsx
+++ src/pages/Portfolios.tsx
@@ -4,13 +4,13 @@ import { Title, Menu } from '../components';
 import { menuItems } from '../data';
 
 const Portfolios: React.FC = (): JSX.Element => {
-  const [menuItem, setMenuItem] = useState('');
+  const [menuItemS, setMenuItemS] = useState(menuItems);
 
   return (
     <MainLayout>
       <Title title="Portfolios" span="Portfolios" />
       <InnerLayout>
-        <Menu menuItems={menuItems} />
+        <Menu menuItems={menuItemS} />
       </InnerLayout>
     </MainLayout>
   );
